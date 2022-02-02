a290a655ee4523d2b1c52efb91623e3276ae5f23
Elio Gerges <elio-gerges@outlook.com>
2022-02-02 16:20:31 -0500

    Configuring project essentials.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './colors.css';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// Header Component
+import Header from './components/Header/Header'
+
+// Footer Component
+import Footer from './components/Footer/Footer'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header/>
+    
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
