d6b1131c5b576d4002ddd18e31246d8a0fa2cb40
Elio Gerges <elio-gerges@outlook.com>
2022-02-03 00:06:48 -0500

    Update header to drop opacity on scroll

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,25 @@ import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 
-function header() {
+// Importing React useRef and useEffect
+import { useRef, useEffect } from 'react';
+
+function Header() {
+
+    const navbar = useRef();
+
+    useEffect(() => {
+        document.addEventListener('scroll', () =>{
+            if(navbar.current.offsetTop > 0) {
+                navbar.current.style.backgroundColor = 'var(--secondary-bg-color-dropped-opacity)';
+            } else {
+                navbar.current.style.backgroundColor = 'var(--secondary-bg-color)';
+            }
+        });
+    });
+
     return(
-        <Navbar className='header-bar' variant='light' sticky='top' expand="lg">
+        <Navbar className='header-bar' variant='light' sticky='top' expand="lg" ref={navbar}>
             <Container fluid>
                 <Navbar.Brand href="/" className='header-logo'>Elio Gerges</Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
@@ -20,4 +36,4 @@ function header() {
     )
 }
 
-export default header;
+export default Header;
